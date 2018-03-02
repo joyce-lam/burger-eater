61dd01d60d691632149f0d28b0fd739580248ab6
Joyce <joycelamsaungan@gmail.com>
2018-03-02 09:20:47 -0800

    adding seeds.sql for database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("BLT");
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
+INSERT INTO burgers (burger_name) VALUES ("Double Quarter");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", true);
