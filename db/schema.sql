a8c091150bac0bd6bb0edffc77ca1a9a145f9910
Joyce <joycelamsaungan@gmail.com>
2018-03-02 09:10:25 -0800

    adding schema for database

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+##schema for database
+CREATE DATABASE burgers_db;
+USE DATABASE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
