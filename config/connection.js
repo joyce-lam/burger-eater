38d3602147cfa825c059929b0412007a6a016d35
Joyce <joycelamsaungan@gmail.com>
2018-03-04 15:49:33 -0800

    modifying conenction for hosting on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 //set up mysql connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+var connection;
 
-connection.connect(function(err) {
-	if (err) {
-		console.error("error connecting: " + err.stack);
-    return;
-	} 
-	console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "hacktheplanet",
+		database: "todogain_db"
+	});
+}
+
+connection.connect();
 
 module.exports = connection;
