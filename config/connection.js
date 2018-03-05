b5c54b94c13e607edded45ff6c19635c368bd751
Joyce <joycelamsaungan@gmail.com>
2018-03-04 21:58:48 -0800

    adding styling elements to html

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,40 @@
 //set up mysql connection
 var mysql = require("mysql");
 
-var connection;
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-		host: "localhost",
-		user: "root",
-		password: "hacktheplanet",
-		database: "todogain_db"
-	});
-}
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-connection.connect();
+
+
+
+// var connection;
+
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+// 	connection = mysql.createConnection({
+// 		host: "localhost",
+// 		user: "root",
+// 		password: "hacktheplanet",
+// 		database: "todogain_db"
+// 	});
+// }
+
+// connection.connect();
 
 module.exports = connection;
+
