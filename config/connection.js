bb3e7b41da53a29b8246e6531ec218d50d9702f2
Joyce <joycelamsaungan@gmail.com>
2018-03-04 23:04:49 -0800

    formatting

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,6 @@
 //set up mysql connection
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-
-
-
 var connection;
 
 if (process.env.JAWSDB_URL) {
